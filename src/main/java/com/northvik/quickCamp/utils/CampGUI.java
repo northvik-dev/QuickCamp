814255d54986d12da4f90f3e0dbc5c5962e94f46
viktorhenchiev <viktorhenchiev@gmail.com>
2025-03-11 23:25:26 +0000

    Changed api version for version capability

--- src/main/java/com/northvik/quickCamp/utils/CampGUI.java
+++ src/main/java/com/northvik/quickCamp/utils/CampGUI.java
@@ -24,29 +24,29 @@ public class CampGUI {
         //non usable area
         ItemStack blackGlass = new ItemStack(Material.BLACK_STAINED_GLASS_PANE);
         ItemMeta blackGlassMeta = blackGlass.getItemMeta();
-        blackGlassMeta.setItemName(" ");
+        blackGlassMeta.setDisplayName(" ");
         blackGlass.setItemMeta(blackGlassMeta);
 
         // Save button
         ItemStack saveButton = getCustomSkull("a79a5c95ee17abfef45c8dc224189964944d560f19a44f19f8a46aef3fee4756");
         ItemMeta saveButtonMeta = saveButton.getItemMeta();
-        saveButtonMeta.setItemName("Save setup");
+        saveButtonMeta.setDisplayName("Save setup");
         saveButton.setItemMeta(saveButtonMeta);
 
         // Close button
         ItemStack closeButton = getCustomSkull("27548362a24c0fa8453e4d93e68c5969ddbde57bf6666c0319c1ed1e84d89065");
         ItemMeta closeButtonMeta = closeButton.getItemMeta();
-        closeButtonMeta.setItemName("Close");
+        closeButtonMeta.setDisplayName("Close");
         closeButton.setItemMeta(closeButtonMeta);
         //Clear button
         ItemStack clearButton = getCustomSkull("cb067ae612d5256a24ccfc74c11814f01962b4d81817a618134b45f36fe6fcb3");
         ItemMeta clearButtonMeta = clearButton.getItemMeta();
-        clearButtonMeta.setItemName("Clear");
+        clearButtonMeta.setDisplayName("Clear");
         clearButton.setItemMeta(clearButtonMeta);
         //Info button
         ItemStack infoButton = getCustomSkull("2705fd94a0c431927fb4e639b0fcfb49717e412285a02b439e0112da22b2e2ec");
         ItemMeta infoButtonMeta = infoButton.getItemMeta();
-        infoButtonMeta.setItemName("Info");
+        infoButtonMeta.setDisplayName("Info");
         infoButtonMeta.setLore(Arrays.asList(
                 ChatColor.GRAY + "Use Clear button to clear blueprint configs table.",
                 ChatColor.GRAY + "Use Save button to save blueprint to config file.",
